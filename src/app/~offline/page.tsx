5302d9cc3901d7e3ea86e9cb63bb54836cc37791
Agung Setyawan <agung.01setyawan@gmail.com>
2026-04-19 16:02:16 +0700

    fix: remove onClick on ~offline page

--- src/app/~offline/page.tsx
+++ src/app/~offline/page.tsx
@@ -9,13 +9,6 @@ export default function OfflinePage() {
     <div className="text-center mt-5">
       <h1>Kamu sedang Offline</h1>
       <p>Halaman ini belum tersedia tanpa koneksi internet.</p>
-      <button
-        type="button"
-        onClick={() => window.location.reload()}
-        className="mt-4"
-      >
-        Coba Lagi
-      </button>
     </div>
   );
 }
