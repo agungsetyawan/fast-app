b3fc0b9308610d61d73284946180a485f38c1c4c
viskawiradi <fastrevamp@gmail.com>
2026-04-22 21:04:08 +0700

    feat: report feature

--- src/app/app/report/page.tsx
+++ src/app/app/report/page.tsx
@@ -2,12 +2,28 @@ import { CircleX } from "lucide-react";
 import { Suspense } from "react";
 import Loading from "@/components/ui/loading";
 import { createClient } from "@/lib/supabase/server";
+import ReportTab from "./component/reporttab";
+import { redirect } from "next/navigation";
 
-async function SimulasiBudgetData() {
+async function UserDetails() {
   const supabase = await createClient();
-  const { data: simulasiBudget, error } = await supabase
-    .from("simulasi_budget")
-    .select();
+  const { data, error } = await supabase.auth.getClaims();
+
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  }
+
+  // return JSON.stringify(data.claims, null, 2);
+  return data.claims;
+}
+
+async function SimulasiCreditData(createdBy: string | undefined,
+  year: string = "2026",) {
+  const supabase = await createClient();
+  const { data: simulasiCredit, error } = await supabase
+    .from("simulasi_kredit")
+    .select()
+    .eq("created_by", createdBy)
 
   if (error) {
     return (
@@ -18,74 +34,46 @@ async function SimulasiBudgetData() {
     );
   }
 
-  return (
-    <table className="table  table-pin-rows table-pin-cols">
-      <thead>
-        <tr>
-          <th>customer_name</th>
-          <td>paket_name</td>
-          <td>area</td>
-          <td>branch</td>
-          <td>asuransi_kendaraan</td>
-          <td>asuransi_jiwa</td>
-          <td>tenor</td>
-          <td>model_kendaraan</td>
-          <td>jenis_kendaraan</td>
-          <td>tipe_kendaraan</td>
-          <td>dp</td>
-          <td>tdp</td>
-          <td>percent_dp</td>
-          <td>nilai_asuransi_kendaraan</td>
-          <td>nilai_asuransi_jiwa</td>
-          <td>angsuran</td>
-          <td>biaya_admin</td>
-          <td>provisi</td>
-          <td>dic</td>
-          <td>tipe_perhitungan</td>
-          <td>created_at</td>
-        </tr>
-      </thead>
-      <tbody>
-        {simulasiBudget.map((budget) => {
-          return (
-            <tr key={budget.id}>
-              <th>{budget.customer_name}</th>
-              <td>{budget.paket_name}</td>
-              <td>{budget.area}</td>
-              <td>{budget.branch}</td>
-              <td>{budget.asuransi_kendaraan}</td>
-              <td>{budget.asuransi_jiwa}</td>
-              <td>{budget.tenor}</td>
-              <td>{budget.model_kendaraan}</td>
-              <td>{budget.jenis_kendaraan}</td>
-              <td>{budget.tipe_kendaraan}</td>
-              <td>{budget.dp}</td>
-              <td>{budget.tdp}</td>
-              <td>{budget.percent_dp}</td>
-              <td>{budget.nilai_asuransi_kendaraan}</td>
-              <td>{budget.nilai_asuransi_jiwa}</td>
-              <td>{budget.angsuran}</td>
-              <td>{budget.biaya_admin}</td>
-              <td>{budget.provisi}</td>
-              <td>{budget.dic}</td>
-              <td>{budget.tipe_perhitungan}</td>
-              <td>{budget.created_at}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  return simulasiCredit;
 }
 
-export default function Page() {
+async function SimulasiBudgetData(createdBy: string | undefined,
+  year: string = "2026",) {
+  const supabase = await createClient();
+  const { data: simulasiBudget, error } = await supabase
+    .from("simulasi_budget")
+    .select()
+    .eq("created_by", createdBy)
+
+  if (error) {
+    return (
+      <div role="alert" className="alert alert-error w-full">
+        <CircleX />
+        <span>{error?.message || "Error!"}</span>
+      </div>
+    );
+  }
+
+  return simulasiBudget;
+}
+
+export default async function Page() {
+
+  const claims = await UserDetails();
+  const createdBy = claims?.email;
+  const year = new Date().getFullYear().toString();
+
+  const budget = await SimulasiBudgetData(createdBy, year);
+  const credit = await SimulasiCreditData(createdBy, year);
+
   return (
     <div className="flex-1 w-full flex flex-col gap-12 max-md:px-4">
-      <div className="flex flex-col gap-2 items-start">
+      <div className="overflow-x-auto h-[calc(100vh-170px)] w-full">
         <Suspense fallback={<Loading />}>
-          <div className="overflow-x-auto h-[calc(100vh-170px)] w-full">
-            <SimulasiBudgetData />
-          </div>
+          <ReportTab
+            simulasiBudget={budget}
+            simulasiCredit={credit}
+          />
         </Suspense>
       </div>
     </div>
